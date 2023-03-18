aa4510fff050e7e690d7293b18883baec8989e53
cuber-3x3 <127592093+cuber-3x3@users.noreply.github.com>
2023-03-18 08:20:48 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,35 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber > 跳绳个数) {
+        跳绳个数 = receivedNumber
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    正在跳绳 = 1
+    basic.showLeds(`
+        # # # # #
+        . # # # .
+        . . # . .
+        . # # # .
+        # # # # #
+        `)
+    radio.sendString("START")
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "FINISH") {
+        正在跳绳 = 0
+        basic.showNumber(跳绳个数)
+    }
+})
+let 正在跳绳 = 0
+let 跳绳个数 = 0
+跳绳个数 = 0
+正在跳绳 = 0
+radio.setGroup(1)
+basic.forever(function () {
+    if (正在跳绳 == 1) {
+        serial.writeValue("x", 跳绳个数)
+        basic.pause(200)
+    } else {
+        basic.showNumber(跳绳个数)
+    }
+})
